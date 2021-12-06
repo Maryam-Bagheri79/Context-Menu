4e228d88b219add5fbaec10249ab9c73428ab826
Maryam-Bagheri79 <m.bagheri7939@gmail.com>
2021-12-06 18:34:52 +0330

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+﻿let $ = document
+
+let clickRight = $.querySelector('#contextMenu')
+
+function contextHandler(event) {
+
+    event.preventDefault();
+
+    if (clickRight.style.display === 'none') {
+
+        clickRight.style.display = 'flex'
+
+        clickRight.style.left = event.pageX + 'px'
+
+        clickRight.style.top = event.pageY + 'px'
+
+    } else {
+
+        clickRight.style.left = event.pageX + 'px'
+
+        clickRight.style.top = event.pageY + 'px'
+
+    }
+
+}
+
+function removeMenu() {
+
+    clickRight.style.display = 'none'
+
+    clickRight.style.removeProperty('left')
+
+    clickRight.style.removeProperty('top')
+
+}
+
+function keyDownHandleer(event) {
+
+    if (event.keyCode === 27) {
+
+        removeMenu();
+
+    }
+}
+
+$.body.addEventListener('contextmenu', contextHandler)
+
+$.body.addEventListener('keydown', keyDownHandleer)
+
+$.body.addEventListener('click', removeMenu)
